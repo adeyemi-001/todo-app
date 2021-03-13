8358a39b7c2a13a9def27af924ac1842b99b28a7
adeyemi sobaki <adeyemi.sobakiqgmail.com>
2021-03-13 23:04:54 +0100

    new

--- todo-app/src/main/java/com/sobaks/todoapp/web/TodoController.java
+++ todo-app/src/main/java/com/sobaks/todoapp/web/TodoController.java
@@ -2,6 +2,7 @@ package com.sobaks.todoapp.web;
 
 import org.springframework.web.bind.annotation.RestController;
 
+//hosted on http://localhost:8080
 @RestController
 public class TodoController {
 
