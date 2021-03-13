87e5c967a6deff80ec0572351cabca01af55196e
adeyemi sobaki <adeyemi.sobakiqgmail.com>
2021-03-13 23:29:14 +0100

    Todo Controller will be finished later (13.03)

--- todo-app/src/main/java/com/sobaks/todoapp/web/TodoController.java
+++ todo-app/src/main/java/com/sobaks/todoapp/web/TodoController.java
@@ -1,9 +1,19 @@
 package com.sobaks.todoapp.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //hosted on http://localhost:8080
 @RestController
 public class TodoController {
-
+  
+	 // i.e fetch the todo Items from the database
+	 @GetMapping("/api/todoItems")
+	 public ResponseEntity<?> fetchAllTodoItems (){
+		 
+		 
+		 
+		 
+	 }
 }
